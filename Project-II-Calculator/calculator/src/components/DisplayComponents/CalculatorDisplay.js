a4801bc0c3488c6ef930359f64ea8b0fafb871fc
Frank Martinez <frankie.martinez@gmail.com>
2019-02-12 15:52:18 -0800

    Calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,29 @@
 import React from 'react';
-import './Display.css';
+import '../../css/index.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
+
+const CalculatorDisplay = (props) => {
+  return ( 
+    <React.Fragment>
+
+
+      <section className="calculator-display"> 
+        <h1> {props.state} </h1>
+        <h1>0</h1>
+      </section>
+  
+      <section className="number-pad">
+        <NumberButton displayTotal={props.state} />
+        <ActionButton />
+      </section>
+
+
+    </React.Fragment>
+  );
+}
+
+
+export default CalculatorDisplay;
+
+
